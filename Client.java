2f9214c880692cb2fcf428ed6cfab465ee7bf2e8
ramigb <pro.rami.gb@gmail.com>
2023-05-18 22:16:26 +0200

    first commit

---
+++ Client.java
@@ -0,0 +1,233 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+public class Client extends JFrame {
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private String playerName;
+    private JLabel timerLabel;
+    private JLabel questionLabel;
+    private JTextField answerField;
+    private JButton submitButton;
+    private JLabel imageLabel;
+    private Timer timer;
+    private int remainingTime = 120;
+
+    public Client(String serverAddress, int serverPort) {
+        try {
+            socket = new Socket(serverAddress, serverPort);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("Error connecting to the server: " + e.getMessage());
+            System.exit(1);
+        }
+
+        initializeUI();
+
+        playerName = JOptionPane.showInputDialog("Enter your name:");
+        writer.println(playerName);
+
+        new ServerListener().start();
+
+        setVisible(true);
+    }
+
+    private void initializeUI() {
+        setTitle("Blind Test Client");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        timerLabel = new JLabel("Time Remaining: -");
+        questionLabel = new JLabel("Waiting for the game to start...");
+        answerField = new JTextField(20);
+        submitButton = new JButton("Submit");
+        imageLabel = new JLabel();
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                submitAnswer();
+            }
+        });
+
+        add(timerLabel);
+        add(imageLabel);
+        add(questionLabel);
+        add(answerField);
+        add(submitButton);
+        startTimer();
+    }
+
+    private void startTimer() {
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                remainingTime--;
+                timerLabel.setText("Time Remaining: " + remainingTime + "s");
+                if (remainingTime == 0) {
+                    timer.stop();
+                    submitAnswer();
+                }
+            }
+        });
+    }
+
+    private void submitAnswer() {
+        String answer = answerField.getText();
+        if (!answer.isEmpty()) {
+            sendMessage(new Message(MessageType.ANSWER, answer));
+            answerField.setText("");
+            submitButton.setEnabled(false);
+            timer.stop();
+        }
+    }
+
+    public void sendMessage(Message message) {
+        System.out.println((message.toString()));
+        writer.println(message.toString());
+    }
+
+    private void processMessage(String message) {
+        String[] parts = message.split("\\|");
+        MessageType type = MessageType.valueOf(parts[0]);
+        String content = parts[1];
+
+        switch (type) {
+            case IMAGE:
+                showImage(content);
+                break;
+            case QUESTION:
+                showQuestion(content);
+                break;
+            case RESULT:
+                showResult(content);
+                break;
+            case GAME_OVER:
+                showGameOver();
+                break;
+            case SCORE:
+                showScores(content);
+                break;
+        }
+    }
+
+    private void showImage(String imagePath) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ImageIcon imageIcon = new ImageIcon(imagePath);
+                Image image = imageIcon.getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT);
+                imageLabel.setIcon(new ImageIcon(image));
+                imageLabel.revalidate();
+                imageLabel.repaint();
+            }
+        });
+    }
+
+    private void showQuestion(String question) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                questionLabel.setText(question);
+                submitButton.setEnabled(true);
+                timer.start();
+            }
+        });
+    }
+
+    private void showResult(String result) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(Client.this, result);
+            }
+        });
+    }
+
+    private void showGameOver() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(Client.this, "Game Over");
+                System.exit(0);
+            }
+        });
+    }
+
+    private void showScores(String scores) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(Client.this, "Scores:\n" + scores);
+            }
+        });
+    }
+
+    private class ServerListener extends Thread {
+        @Override
+        public void run() {
+            try {
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    System.out.println(message);
+                    processMessage(message);
+                }
+            } catch (IOException e) {
+                System.out.println("Error receiving message from the server: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String serverAddress = "127.0.0.1"; // Change this to the actual server address
+        int serverPort = 8888; // Change this to the actual server port
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Client(serverAddress, serverPort);
+            }
+        });
+    }
+
+    private static class Message {
+        private MessageType type;
+        private Object content;
+
+        public Message(MessageType type, String content) {
+            this.type = type;
+            this.content = content;
+        }
+
+        public Message(MessageType type, List<String> content) {
+            this.type = type;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return type.name() + "|" + content;
+        }
+    }
+
+    private enum MessageType {
+        IMAGE,
+        QUESTION,
+        RESULT,
+        GAME_OVER,
+        SCORE,
+        TIME_OUT,
+        ANSWER
+    }
+}
---
+++ Server.java
@@ -0,0 +1,309 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private static final int PORT = 8888;
+    private static final int MAX_PLAYERS = 2;
+
+    private ServerSocket serverSocket;
+    private List<ClientHandler> clients;
+    private GameState gameState;
+    private int currentPlayerIndex;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+            clients = new ArrayList<>();
+            gameState = new GameState();
+            currentPlayerIndex = 0;
+
+            System.out.println("Server started on port " + PORT);
+        } catch (IOException e) {
+            System.out.println("Error starting the server: " + e.getMessage());
+        }
+    }
+
+    public void start() {
+        try {
+            while (clients.size() < MAX_PLAYERS) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler);
+                clientHandler.start();
+                System.out.println("Player connected: " + clientHandler.getPlayerName());
+            }
+
+            System.out.println("Game started!");
+            sendImagesToClients();
+            startGame();
+
+        } catch (IOException e) {
+            System.out.println("Error accepting client connection: " + e.getMessage());
+        }
+    }
+
+    private void sendImagesToClients() {
+        // int nextImageIndex = (gameState.getCurrentImageIndex() + 1) %
+        // gameState.getImagePaths().size();
+        String imagePath = gameState.getCurrentImage();
+        for (ClientHandler client : clients) {
+            client.sendMessage(new Message(MessageType.IMAGE, imagePath));
+        }
+    }
+
+    private void startGame() {
+        while (!gameState.isGameOver()) {
+            ClientHandler currentPlayer = clients.get(currentPlayerIndex);
+            sendImagesToClients();
+            currentPlayer.sendMessage(new Message(MessageType.QUESTION, "Who is this player?"));
+            currentPlayer.startTimer();
+
+            // Wait for the current player's answer
+            String answer = currentPlayer.waitForAnswer();
+
+            if (gameState.isCorrectAnswer(answer)) {
+                currentPlayer.incrementScore();
+                currentPlayer.sendMessage(new Message(MessageType.RESULT, "Correct!"));
+            } else {
+                currentPlayer.sendMessage(new Message(MessageType.RESULT,
+                        "Wrong! The correct answer is: " + gameState.getCurrentAnswer()));
+            }
+
+            currentPlayer.stopTimer();
+            if (currentPlayerIndex == 1) {
+                gameState.setCurrentImageIndex(gameState.getCurrentImageIndex() + 1);
+            }
+
+            currentPlayerIndex = (currentPlayerIndex + 1) % clients.size();
+        }
+
+        // Game over
+        sendGameOverMessages();
+        closeConnections();
+    }
+
+    private void sendGameOverMessages() {
+        List<Integer> scores = new ArrayList<>();
+        for (ClientHandler client : clients) {
+            scores.add(client.getScore());
+        }
+        for (ClientHandler client : clients) {
+            client.sendMessage(new Message(MessageType.GAME_OVER, "Game over! Final scores:"));
+            client.sendMessage(new Message(MessageType.SCORE, gameState.getScores(scores.get(0), scores.get(1))));
+        }
+    }
+
+    private void closeConnections() {
+        try {
+            for (ClientHandler client : clients) {
+                client.close();
+            }
+            serverSocket.close();
+        } catch (IOException e) {
+            System.out.println("Error closing connections: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
+    private static class GameState {
+        private List<String> imagePaths;
+        private List<String> answers;
+        private int currentImageIndex;
+
+        public int getCurrentImageIndex() {
+            return currentImageIndex;
+        }
+
+        public void setCurrentImageIndex(int index) {
+            currentImageIndex = index;
+        }
+
+        public GameState() {
+            imagePaths = new ArrayList<>();
+            answers = new ArrayList<>();
+
+            // Add image paths and answers
+            imagePaths.add("/Users/ramigb/Desktop/test_res/resources/player1.jpg");
+            answers.add("Player 1");
+
+            imagePaths.add("/Users/ramigb/Desktop/test_res/resources/player2.jpg");
+            answers.add("Player 2");
+
+            // ... Add more image paths and answers as needed
+
+            currentImageIndex = 0;
+        }
+
+        public String getCurrentImage() {
+            return imagePaths.get(currentImageIndex);
+        }
+
+        public boolean isCorrectAnswer(String answer) {
+            String correctAnswer = answers.get(currentImageIndex);
+            return answer.equalsIgnoreCase(correctAnswer);
+        }
+
+        public String getCurrentAnswer() {
+            return answers.get(currentImageIndex);
+        }
+
+        public boolean isGameOver() {
+            return currentImageIndex >= imagePaths.size();
+        }
+
+        public List<String> getScores(int score1, int score2) {
+            List<String> scores = new ArrayList<>();
+            scores.add("* Player 1: " + score1);
+            scores.add("* Player 2: " + score2);
+            return scores;
+        }
+    }
+
+    private void processMessage(String message) {
+        String[] parts = message.split("\\|");
+        MessageType type = MessageType.valueOf(parts[0]);
+        String content = parts[1];
+        ClientHandler currentPlayer = clients.get(currentPlayerIndex);
+
+        switch (type) {
+            case ANSWER:
+                if (gameState.isCorrectAnswer(content)) {
+                    currentPlayer.incrementScore();
+                    currentPlayer.sendMessage(new Message(MessageType.RESULT, "Correct!"));
+                } else {
+                    currentPlayer.sendMessage(new Message(MessageType.RESULT,
+                            "Wrong! The correct answer is: " + gameState.getCurrentAnswer()));
+                }
+
+                currentPlayer.stopTimer();
+                if (currentPlayerIndex == 1) {
+                    gameState.setCurrentImageIndex(gameState.getCurrentImageIndex() + 1);
+                }
+
+                currentPlayerIndex = (currentPlayerIndex + 1) % clients.size();
+                break;
+            case TIME_OUT:
+                break;
+            default:
+                return;
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+        private String playerName;
+        private int score;
+        private boolean isTimerRunning;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+            score = 0;
+            isTimerRunning = false;
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            } catch (IOException e) {
+                System.out.println("Error creating I/O streams for client: " + e.getMessage());
+            }
+        }
+
+        public String getPlayerName() {
+            return playerName;
+        }
+
+        public void sendMessage(Message message) {
+            writer.println(message.toString());
+        }
+
+        public void incrementScore() {
+            score++;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void startTimer() {
+            isTimerRunning = true;
+        }
+
+        public void stopTimer() {
+            isTimerRunning = false;
+        }
+
+        public String waitForAnswer() {
+            try {
+                while (true) {
+                    String answer = reader.readLine();
+                    if (answer != null) {
+                        String[] parts = answer.split("\\|");
+                        return parts[1];
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading client input: " + e.getMessage());
+            }
+
+            return null;
+        }
+
+        public void close() {
+            try {
+                reader.close();
+                writer.close();
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Error closing client connection: " + e.getMessage());
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                playerName = reader.readLine();
+            } catch (IOException e) {
+                System.out.println("Error reading player name: " + e.getMessage());
+            }
+        }
+    }
+
+    private static class Message {
+        private MessageType type;
+        private Object content;
+
+        public Message(MessageType type, String content) {
+            this.type = type;
+            this.content = content;
+        }
+
+        public Message(MessageType type, List<String> content) {
+            this.type = type;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return type.name() + "|" + content;
+        }
+    }
+
+    private enum MessageType {
+        IMAGE,
+        QUESTION,
+        RESULT,
+        GAME_OVER,
+        SCORE,
+        TIME_OUT,
+        ANSWER
+    }
+}
